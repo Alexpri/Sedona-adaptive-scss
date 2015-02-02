184db379201c23b2f9559d82b6a6b37cdee62545
Alexandr Priadko <alexandrpriad@gmail.com>
2015-02-02 19:26:31 +0200

    min2

--- js/main.js
+++ js/main.js
@@ -23,30 +23,3 @@ link.addEventListener('click', function() {
   ".menu".active = classes.join("");
 };
 });*/
-
-
-
-
-
-/*var menubtn = document.querySelector(".menu-btn");
-var active = document.querySelector(".menu");
-
-menubtn.addEventListener("click", function (event) {
-	event.preventDefault();
-	if (".menu")
-	active.classList.add("active");
-});
-*/
-/*if (".menu".classList) {
-  (".menu").classList.toggle("active");
-} else {
-  var classes = (".menu").className.split(" ");
-  var existingIndex = classes.indexOf("active");
-
-  if (existingIndex >= 0)
-    classes.splice(existingIndex, 1);
-  else
-    classes.push("active");
-
-  (".menu").className = classes.join(' ');
-};*/
